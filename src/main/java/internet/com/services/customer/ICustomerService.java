6280a42f4206dd4185f01da075d29384a1824dd3
Thienduy0195 <thienduy0195@gmail.com>
2022-08-13 12:31:08 +0700

    DuyNT update customer with method findby id

--- src/main/java/internet/com/services/customer/ICustomerService.java
+++ src/main/java/internet/com/services/customer/ICustomerService.java
@@ -1,5 +1,16 @@
 package internet.com.services.customer;
 
+import internet.com.entity.customer.Customer;
+
+import java.util.Optional;
+
 public interface ICustomerService {
+
+    /**
+     * Created by: DuyNT
+     * Date Created: 10/08/2022
+     * @param id
+     */
+    Optional<Customer> findCustomerById(Integer id);
 
 }
--- src/main/java/internet/com/services/customer/impl/CustomerService.java
+++ src/main/java/internet/com/services/customer/impl/CustomerService.java
@@ -1,8 +1,20 @@
 package internet.com.services.customer.impl;
 
+import internet.com.entity.customer.Customer;
+import internet.com.repository.customer_repo.ICustomerRepository;
 import internet.com.services.customer.ICustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CustomerService implements ICustomerService {
+    @Autowired
+    ICustomerRepository customerRepository;
+
+    @Override
+    public Optional<Customer> findCustomerById(Integer id) {
+        return this.customerRepository.findByIdCustomer(id);
+    }
 }
