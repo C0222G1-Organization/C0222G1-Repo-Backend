09a78c0a1affde8c461ea439e45dd8b1d630347e
C0222G1-NguyenHoangHao <hoanghao9x87@gmail.com>
2022-08-13 13:32:44 +0700

    HaoNH update and merge createCustomer method to customer-management

--- src/main/java/internet/com/repository/customer_repo/ICustomerRepository.java
+++ src/main/java/internet/com/repository/customer_repo/ICustomerRepository.java
@@ -1,6 +1,7 @@
 package internet.com.repository.customer_repo;
 
 import internet.com.entity.customer.Customer;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -20,5 +21,49 @@ public interface ICustomerRepository extends CrudRepository<Customer, Integer> {
     @Query(value = "SELECT id, customer_name, date_of_birth,phone_number, email, active_status, delete_status, " +
             "remaining_time, user_name, address_id FROM customer WHERE id =:customerId", nativeQuery = true)
     Optional<Customer> findByIdCustomer(@Param("customerId") Integer customerId);
+
+    /**
+     * Created by: HaoNH
+     * Date Created: 09/06/2022
+     * method save customer
+     *
+     * @param name
+     * @param dateOfBirth
+     * @param email
+     * @param phone
+     * @param userName
+     * @param address
+     */
+    @Modifying
+    @Query(value = "INSERT INTO customer(customer_name, date_of_birth, email, phone_number, remaining_time, user_name, address_id) VALUE " +
+            "( :name, :dateOfBirth, :email, :phone, 0, :userName, :address)", nativeQuery = true)
+    void saveCustomer(@Param("name") String name,
+                      @Param("dateOfBirth") String dateOfBirth,
+                      @Param("email") String email,
+                      @Param("phone") String phone,
+                      @Param("userName") String userName,
+                      @Param("address") Integer address);
+
+    /**
+     * Create by HaoNH
+     * Date create: 11/09/2022
+     * method check email is exits
+     *
+     * @param email
+     * @return
+     */
+    @Query(value = "SELECT email FROM customer where email = :email", nativeQuery = true)
+    String existsEmail(@Param("email") String email);
+
+    /**
+     * Create by HaoNH
+     * Date create: 11/09/2022
+     * method check phone is exits
+     *
+     * @param phone
+     * @return
+     */
+    @Query(value = "SELECT phone_number FROM customer where phone_number = :phone", nativeQuery = true)
+    String existsPhone(@Param("phone") String phone);
 }
 
