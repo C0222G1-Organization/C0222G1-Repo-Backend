d20d64ab6e58c948a0d8a2a99453088a15438d01
tuan2904 <dtuan290400@gmail.com>
2022-08-13 16:25:00 +0700

    TuanHD update source code BE

--- src/main/java/internet/com/services/computer/IComputerTypeService.java
+++ src/main/java/internet/com/services/computer/IComputerTypeService.java
@@ -1,5 +1,9 @@
 package internet.com.services.computer;
 
-public interface IComputerTypeService {
+import internet.com.entity.computer.ComputerType;
 
+import java.util.List;
+
+public interface IComputerTypeService {
+    List<ComputerType> findAll();
 }
