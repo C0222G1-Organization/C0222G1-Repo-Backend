2ee3b103fd6a2f2553eca004bb95d4126d103726
FabulousNBL <nguyenblong2412@gmail.com>
2022-08-15 15:07:48 +0700

    LongNB update

--- src/main/java/internet/com/services/record/IRecordService.java
+++ src/main/java/internet/com/services/record/IRecordService.java
@@ -1,11 +1,11 @@
-package internet.com.services.record;
-
-import internet.com.dto.user_dto.response.JWTResponseCustomer;
-import internet.com.entity.record.Record;
-
-public interface IRecordService {
-
-    JWTResponseCustomer createRecord(Integer customerId);
-
-    void setEndTime(Integer id, String endTime);
-}
+//package internet.com.services.record;
+//
+//import internet.com.dto.user_dto.response.JWTResponseCustomer;
+//import internet.com.entity.record.Record;
+//
+//public interface IRecordService {
+//
+//    JWTResponseCustomer createRecord(Integer customerId);
+//
+//    void setEndTime(Integer id, String endTime);
+//}
